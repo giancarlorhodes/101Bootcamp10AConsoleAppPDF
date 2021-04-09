1e73915dfedf22c5cf1933477f90c784e45cdf92
Giancarlo Rhodes <giancarlo.rhodes@onshoreoutsourcing.com>
2021-04-09 14:05:44 -0500

    Add project files.

---
+++ 101Bootcamp10AConsoleAppPDF/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Shark.PdfConvert;
+
+namespace _101Bootcamp10AConsoleAppPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Attempting to create a PDF using Shark.PdfConvert");          
+            Console.WriteLine("What is the name for your pdf?");
+            string fileName = Console.ReadLine();
+            string filePath = @"t:\temp\" + fileName + ".pdf";
+            Console.WriteLine("Enter the text for the paragraph tag");
+            string paragraphText = Console.ReadLine();
+            Console.WriteLine("Enter the text for the h1 tag");
+            string h1Text = Console.ReadLine();
+
+            string content = @"<!DOCTYPE html><html><body><h1>" + h1Text + 
+                @"</h1><p>" + paragraphText + @"</p></body></html>";
+
+            try
+            {
+
+                PdfConvert.Convert(new Shark.PdfConvert.PdfConversionSettings
+                {
+
+                    Title = "My First Attempt At Using Shark.PdfConvert",
+                    Content = content,
+                    OutputPath = filePath
+
+                });
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                // throw;
+            }
+
+           
+
+            Console.WriteLine("Converted successfully and PDF is at " + filePath);
+            Console.ReadLine();
+        }
+    }
+}
